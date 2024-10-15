c1ca249c40373f171cfebfdc77c8504388f18452
Jeremy Delannoy <jeremydelannoy.jd@gmail.com>
2024-10-15 16:46:26 +0200

    Mise à jour swagger et profil
    Modifications du swagger
    Modifications de profil :
    - rétablissement de l'affichage des comptes bancaires
    - Modification du prénom et nom :
      - Formulaire > bouton submit> handle submit >
       dispatch > store

--- frontend/src/_Redux/Slices/userSlice.js
+++ frontend/src/_Redux/Slices/userSlice.js
@@ -1,20 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    firstName: '',
-    lastName: '',
-}
+  firstName: '',
+  lastName: '',
+};
 
 export const userSlice = createSlice({
-    name: "User",
-    initialState,
-    reducers: {
-        setUserProfile: (state, action) => {
-            state.firstName = action.payload.firstName;
-            state.lastName = action.payload.lastName;
-        },
+  name: "User",
+  initialState,
+  reducers: {
+    setUserProfile: (state, action) => {
+      state.firstName = action.payload.firstName;
+      state.lastName = action.payload.lastName;
     },
+  },
+});
 
-}
+// Export des actions pour qu'elles puissent être utilisées dans d'autres fichiers
+export const { setUserProfile } = userSlice.actions;
 
-)
+// Export du reducer par défaut
+export default userSlice.reducer;
--- frontend/src/pages/public/Profil.jsx
+++ frontend/src/pages/public/Profil.jsx
@@ -3,7 +3,8 @@ import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { accountService } from '@/_service/accountService';
 import { userSelector } from '@/_Redux/selector/userSelector';
-import accountsData from "@/data/accountsData.json";
+import { setUserProfile } from '@/_Redux/Slices/userSlice';
+import accountsData from '@/data/accountsData.json';
 
 const Profil = () => {
   const { token } = accountService.getToken();
@@ -14,35 +15,65 @@ const Profil = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  // Récupération des infos de l'utilisateur
   const setInfos = async () => {
     const decodedToken = await accountService.getProfile(token);
-    dispatch({ type: 'User/setUserProfile', payload: { firstName: decodedToken.firstName, lastName: decodedToken.lastName } });
+    dispatch(setUserProfile({ firstName: decodedToken.firstName, lastName: decodedToken.lastName }));
   };
 
+  // Synchronisation des inputs avec les données du store
+  useEffect(() => {
+    setNewFirstName(firstName);
+    setNewLastName(lastName);
+  }, [firstName, lastName]);
+
+  // Vérification si l'utilisateur est connecté
   useEffect(() => {
     if (accountService.isLogged()) {
       setInfos();
-      setNewFirstName(firstName);
-      setNewLastName(lastName);
     } else {
       navigate('/');
     }
-  }, [firstName, lastName]);
+  }, []);
+
+  // Gestion de la soumission du formulaire
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submitting:", { firstName: newFirstName, lastName: newLastName });
+
+    dispatch(setUserProfile({ firstName: newFirstName, lastName: newLastName }));
+
+    // Log pour confirmer la mise à jour dans le store
+    console.log("Store updated:", { firstName: newFirstName, lastName: newLastName });
+  };
 
   return (
     <div className="main">
       <div className="info">
         <h1>Welcome back</h1>
-        <div className="input-group">
-          <input type="text" value={newFirstName} onChange={(e) => setNewFirstName(e.target.value)} />
-          <input type="text" value={newLastName} onChange={(e) => setNewLastName(e.target.value)} />
-        </div>
-        <div className="button-group">
-          <button className="edit-button">Save</button>
-          <button className="edit-button">Cancel</button>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <input
+              type="text"
+              value={newFirstName}
+              onChange={(e) => setNewFirstName(e.target.value)}
+              placeholder="First Name"
+            />
+            <input
+              type="text"
+              value={newLastName}
+              onChange={(e) => setNewLastName(e.target.value)}
+              placeholder="Last Name"
+            />
+          </div>
+          <div className="button-group">
+            <button type="submit" className="edit-button">Save</button>
+            <button type="button" className="edit-button">Cancel</button>
+          </div>
+        </form>
       </div>
 
+      {/* Affichage des comptes bancaires */}
       {accountsData.map((account) => (
         <section key={account.id} className="account">
           <div className="account-content-wrapper">
